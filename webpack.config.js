1f6e432fc03e0c5a20da15e87d1074574685e2cd
daixiaotian <daixiaotian@kuaishou.com>
2022-02-16 19:46:59 +0800

    feat: 添加 webpack-dev-server 支持

--- webpack.config.js
+++ webpack.config.js
@@ -83,4 +83,19 @@ module.exports = {
       cleanOnceBeforeBuildPatterns: ["dist"],
     }),
   ],
+  devServer: {
+    port: 8888,
+    static: {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+    allowedHosts: "all",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+    open: true,
+  },
 };
